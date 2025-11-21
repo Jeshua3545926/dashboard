dda0900a36f8d3d6fe3c5d7813bb04484587b1a2
Jeshua3545926 <garzaromerojeshuaabiram2019ktv@gmail.com>
2025-11-20 20:07:55 -0600

    app

---
+++ Actions/action2.ts
@@ -0,0 +1,71 @@
+import { register } from '../types/type';
+
+export abstract class Action2 implements register {
+    id: string;
+    title: string;
+    content: string;
+
+    constructor(props: register) {
+        this.id = props.id;
+        this.title = props.title;
+        this.content = props.content;
+    }
+    // Puede devolver JSX para renderizarlo en un contenedor
+    abstract run(): void | React.ReactNode | string;
+}
+
+const registry = new Map<string, Action2>();
+
+
+export function registerAction(ActionClass: new () => Action2) {
+    const instance = new ActionClass();
+    registry.set(instance.id, instance);
+    if (typeof instance.run !== 'function') {
+        instance.run()
+    }
+
+    const proto =
+        Object.getPrototypeOf(instance);
+    const methodNames = Object.getOwnPropertyNames(proto)
+        .filter(name => typeof (instance as any)[name] === 'function' && name !== 'constructor' && name !== 'run');
+
+
+    for (const name of methodNames) {
+        console.log(`⚡ Ejecutando método adicional: ${name}`);
+        (instance as any)[name](); // ejecuta el método
+    }
+    return instance;
+}
+
+export function getAllActions() {
+    return Array.from(registry.values());
+}
+
+export function runAction(id: string) {
+    const action = registry.get(id);
+    if (!action) {
+        alert(`⚠️ Acción no encontrada: ${id}`);
+        return;
+    }
+
+    const result = action.run();
+
+    // Si la acción devolvió algo, intentamos renderizarlo
+    // if (result !== undefined && result !== null) {
+    //     const container =
+    //         document.getElementById('action-root') ||
+    //         document.getElementById('caps');
+
+    //     if (!container) {
+    //         console.warn('No se encontró contenedor para renderizar la acción (action-root/caps).');
+    //         return;
+    //     }
+
+    //     if (typeof result === 'string') {
+    //         container.innerHTML = result;
+    //     } else {
+    //         const root = createRoot(container);
+    //         root.render(result as React.ReactNode);
+    //     }
+    // }
+}
---
+++ types/type.ts
@@ -0,0 +1,6 @@
+export interface register {
+    id: string;
+    title: string;
+    content: string;
+
+}
